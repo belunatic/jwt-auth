5894de7b6573f7ce46f8019bd63542c79332875f
belunatic <abelluvyou@yahoo.com>
2025-03-12 11:24:26 -0700

    refactor: restructure database connection and user model; add authentication routes

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRoute = require("./routes/auth");
+const connectDB = require("./config/database");
+
+require("dotenv").config({ path: "./config/.env" });
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+connectDB();
+
+app.use("/", async (req, res) => {
+	res.send("I am son cool");
+});
+
+app.listen(process.env.PORT || 5000, () => {
+	console.log("let get this party started 🐯");
+});
---
+++ server/models/User.js
@@ -0,0 +1,24 @@
+// server/models/User.js
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		unique: true,
+	},
+	admin: {
+		type: Boolean,
+		default: false,
+	},
+});
+
+module.exports = mongoose.model("User", UserSchema);
